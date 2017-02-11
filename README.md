f82975f88d06a580e65672ecfa8e448ee64201c2
Luiz Bills <luizpbills@gmail.com>
2017-02-11 18:13:28 +0000

    initial

---
+++ README.md
@@ -0,0 +1,81 @@
+# CSS Generator
+
+Write css programatically using PHP.
+
+## Install
+
+```php
+include_once 'path/to/lib/class-css-generator.php';
+```
+
+## Usage
+
+write this:
+```php
+<?php
+
+$css = new CSS_Generator();
+
+for ($i = 1; $i <= 4; $i++) {
+	$css->add_rule( '.a-' . $i, array(
+		'color' => 'rgb(200, 200,' . ($i * 20) . ')',
+	) );
+}
+
+$css->open_media('screen and (min-width: 30em)');
+
+// multiple selectors
+$css->add_rule( array( 'html', 'body' ), array(
+		'background-color' => 'black',
+		'color' => 'white'
+	)
+);
+
+$css->close_media();
+```
+
+to output this:
+```css
+.a-1{
+	color:rgb(200, 200,20);
+}
+.a-2{
+	color:rgb(200, 200,40);
+}
+.a-3{
+	color:rgb(200, 200,60);
+}
+.a-4{
+	color:rgb(200, 200,80);
+}
+@media screen and (min-width: 30em){
+	html,
+	body{
+		background-color:black;
+		color:white;
+	}
+}
+
+```
+
+If you pass a `true` on constructor, the css generated will be minified. Example:
+```php
+<?php
+$css = new CSS_Generator(true);
+
+$css->open_media('screen and (min-width: 30em)');
+
+$css->add_rule( array( 'html', 'body' ), array(
+		'background-color' => 'black',
+		'color' => 'white'
+	)
+);
+```
+
+outputs:
+```css
+@media screen and (min-width: 30em){html,body{background-color:black;color:white;}}
+```
+
+## License
+MIT License &copy; 2017 Agência Zoop
---
+++ class-css-generator.php
@@ -0,0 +1,65 @@
+<?php
+
+class CSS_Generator {
+
+	private $output = '';
+	private $linebreak = null;
+	private $media_is_open = false;
+
+	public function __construct ( $compact = false ) {
+		$this->linebreak = ! $compact ? PHP_EOL : '';
+	}
+
+	public function get_output ( $close_media = true ) {
+		if ( $this->media_is_open ) {
+			$this->close_media();
+		}
+		return $this->output;
+	}
+
+	public function add_raw ( $string ) {
+		$this->output .= $string;
+	}
+
+	public function add_rule ( $selectors, $declarations_array ) {
+		$declarations = array();
+		$selector_indentation = '';
+		$declaration_indentation = '';
+
+		if ( ! empty( $this->linebreak ) ) {
+			$selector_indentation = $this->media_is_open ? "\t" : '';
+			$declaration_indentation = $this->media_is_open ? "\t\t" : "\t";
+		}
+
+		if ( ! is_array( $selectors ) ) {
+			$selectors = array( $selectors );
+		}
+
+		foreach ( $selectors as $key => $value ) {
+			$selectors[ $key ] = $selector_indentation . $value;
+		}
+
+		foreach ( $declarations_array as $key => $value ) {
+			$declarations[] = $declaration_indentation . $key . ':' . $value . ';' . $this->linebreak;
+		}
+
+		$this->output .= implode( ',' . $this->linebreak, $selectors ) . '{' . $this->linebreak
+			. implode( false, $declarations )
+			. $selector_indentation . '}' . $this->linebreak;
+	}
+
+	public function open_media ( $breakpoint ) {
+		if ( $this->media_is_open ) {
+			$this->close_media();
+		}
+		$this->media_is_open = true;
+		$this->output .= '@media ' . $breakpoint . '{' . $this->linebreak;
+	}
+
+	public function close_media () {
+		if ( $this->media_is_open ) {
+			$this->media_is_open = false;
+			$this->output .= '}' . $this->linebreak;
+		}
+	}
+}
