98ec326c9568689ee48ee8073fb299e6c2f140f2
Luiz Paulo "Bills <luizpbills@gmail.com>
2017-04-23 12:56:29 -0300

    bump version

--- class-css-generator.php
+++ class-css-generator.php
@@ -3,7 +3,7 @@
  * CSS Generator
  * Write css programatically using PHP.
  *
- * @version 1.2.0
+ * @version 1.3.0
  * @author Agência Zoop <hello@agenciazoop.com>
  * @copyright 2017 Agência Zoop
  * @license MIT
